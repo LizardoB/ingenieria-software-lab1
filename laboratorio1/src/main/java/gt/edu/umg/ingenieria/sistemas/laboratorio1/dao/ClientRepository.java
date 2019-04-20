f69bfcb0efbd247d15e5b87655a6150ed9ecbd57
lizardo <lizardo@lizardo-X555LAB>
2019-04-20 17:20:48 -0600

    Laboratorio terminado.

--- laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/dao/ClientRepository.java
+++ laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/dao/ClientRepository.java
@@ -1,9 +1,21 @@
 package gt.edu.umg.ingenieria.sistemas.laboratorio1.dao;
 
+import gt.edu.umg.ingenieria.sistemas.laboratorio1.model.Client;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author Josué Barillas (jbarillas)
  */
-public interface ClientRepository {
+
+@Repository
+public interface ClientRepository extends CrudRepository<Client, Long>{
+    public Client findClientByNit(String nit);
     
+    @Query("select client from Client client where concat(firstName,' ',lastName) like %?1%")
+    List<Client> findByFirstLast(String query);
+
 }
---
+++ laboratorio1/src/main/java/gt/edu/umg/ingenieria/sistemas/laboratorio1/model/ReglasException.java
@@ -0,0 +1,7 @@
+package gt.edu.umg.ingenieria.sistemas.laboratorio1.model;
+
+public class ReglasException extends Exception{
+    public ReglasException(String msg) {
+        super(msg);
+    }
+}
